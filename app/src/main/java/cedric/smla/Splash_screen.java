01ab166b1556f690642fc8dda22c6579512d90cc
cedricSMLA <soumced97625@gmail.com>
2019-12-23 16:43:19 +0100

    Splash screen

---
+++ app/src/main/java/cedric/smla/Splash_screen.java
@@ -0,0 +1,29 @@
+package cedric.smla;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //rediriger vers la page principale après un delay de 2s
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                // demarrer une page
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        // handler post delay
+        new Handler().postDelayed(runnable, 2000);
+    }
+}
